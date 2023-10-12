6095ebbdd4de0e3b850efdc2ae3f843bef5169d6
Kerigo Ndirangu <62352071+kgndirangu@users.noreply.github.com>
2023-10-12 19:52:18 -0400

    added helpers

---
+++ helpers.py
@@ -0,0 +1,2 @@
+#Inside of `helpers.py`, write a function, called `greet`, that accepts a variable called `name` and prints 
+#`"Hello, <name>"`. For example, `greet("Jane")` would print: `"Hello, Jane"`.
